33f1bb295c55d2ebb72fca25139fbe93fb8f1c43
Samuel Y <samuel.y@weimeit.com>
2017-03-13 17:30:25 +0800

    completed

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ port = process.env.PORT || 8080;
 
 router = require('./app/router.js');
 
-app.use(router);
 
+el = require('express-ejs-layouts');
+app.use(eo.static(__dirname + '/public'));
+app.use(el);
+app.set('view engine', 'ejs');
+app.use(router);
 app.listen(port, ()=>{
 	console.log(`Listening on port: ${port}`);
 });
